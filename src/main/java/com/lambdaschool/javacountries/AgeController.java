7cc67d7d44c04863340998c687edf96882707eb1
tthompson082 <tthompson082@gmail.com>
2019-07-15 16:53:09 -0400

    :checkered_flag: MVP Met
    Create Age Controller, MVP met, all necessary endpoints added and functioning

--- src/main/java/com/lambdaschool/javacountries/AgeController.java
+++ src/main/java/com/lambdaschool/javacountries/AgeController.java
@@ -21,4 +21,24 @@ public class AgeController
         ArrayList<Country> rtnCtry = JavacountriesApplication.ourCountryList.findCountries(c -> c.getMedianAge() >= age);
         return new ResponseEntity<>(rtnCtry, HttpStatus.OK);
     }
+
+    // localhost:8080/age/min
+    // return the country with the lowest median age
+    @GetMapping(value = "/min", produces = {"application/json"})
+    public  ResponseEntity<?> smallestMedianAge()
+    {
+        ArrayList<Country> rtnCtry = JavacountriesApplication.ourCountryList.findCountries(c -> c.getMedianAge() > 0);
+        rtnCtry.sort((o1, o2) -> o1.getMedianAge() - o2.getMedianAge());
+        return new ResponseEntity<>(rtnCtry.get(0), HttpStatus.OK);
+    }
+
+    // localhost:8080/age/max
+    // return the country with the lowest median age
+    @GetMapping(value = "/max", produces = {"application/json"})
+    public  ResponseEntity<?> largestMedianAge()
+    {
+        ArrayList<Country> rtnCtry = JavacountriesApplication.ourCountryList.findCountries(c -> c.getMedianAge() > 0);
+        rtnCtry.sort((o1, o2) -> o2.getMedianAge() - o1.getMedianAge());
+        return new ResponseEntity<>(rtnCtry.get(0), HttpStatus.OK);
+    }
 }
